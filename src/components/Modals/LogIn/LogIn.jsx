3197b8390cc39d02cdfd2c6153225bf404324c65
Миша Бабенко <misha77b@gmail.com>
2022-04-18 21:33:34 +0300

    hide and show password

--- src/components/Modals/LogIn/LogIn.jsx
+++ src/components/Modals/LogIn/LogIn.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import TextField from '@mui/material//TextField';
+
 import { logIn } from '../../../API/auth/auth.thunks';
 import PropTypes from 'prop-types';
 
@@ -24,9 +29,10 @@ const validationSchema = yup.object({
     .required('Password is required'),
 });
 
-const LogIn = ({ handleClose }) => {  
-
+const LogIn = ({ handleClose }) => {
   const dispatch = useDispatch();
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -44,6 +50,14 @@ const LogIn = ({ handleClose }) => {
       handleClose();
     },
   });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
     
   return (
     <div>
@@ -62,16 +76,31 @@ const LogIn = ({ handleClose }) => {
               helperText={formik.touched.email && formik.errors.email}
             />
             <TextField
+              margin='normal'
               fullWidth
               id="password"
               name="password"
               label="Password"
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               value={formik.values.password}
               onChange={formik.handleChange}
               error={formik.touched.password && Boolean(formik.errors.password)}
               helperText={formik.touched.password && formik.errors.password}
             />
+
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              sx={{
+                position: 'absolute',
+                top: 127,
+                right: 40
+              }}
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+
             <Button sx={{
                 width: '175px', 
                 margin: '0 auto',
