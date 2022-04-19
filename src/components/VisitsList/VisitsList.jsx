07e10b747f4c935ffbdbe39e8322c752ccdee987
Миша Бабенко <misha77b@gmail.com>
2022-04-20 00:01:57 +0300

    search beginning

--- src/components/VisitsList/VisitsList.jsx
+++ src/components/VisitsList/VisitsList.jsx
@@ -6,6 +6,7 @@ import { setIsLoading } from '../../API/visits/visits.actions';
 
 import { getVisits } from '../../API/visits/visits.thunks';
 import VisitCard from '../VisitCard/VisitCard';
+import CardFilter from '../cardFilter/CardFilter';
 
 const VisitsList = () => {
   
@@ -28,13 +29,15 @@ const VisitsList = () => {
     
   return (
     <>
-      {!isLoading ? <Preloader /> : 
-        <div style={{width: '100%', display: 'flex', justifyContent: 'space-betwen', justifyContent: 'center', flexWrap: 'wrap'}} >
-          {visits.length === 0 ? <h1 style={{margin: '325px auto'}} >No items have been added...</h1> : 
-            <VisitCard 
-              visits={visits}
-            />
-          }
+      {!isLoading ? <Preloader /> : <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>      
+          <CardFilter />
+          <div style={{width: '100%', display: 'flex', justifyContent: 'space-betwen', justifyContent: 'center', flexWrap: 'wrap'}} >
+            {visits.length === 0 ? <h1 style={{margin: '325px auto'}} >No items have been added...</h1> : 
+              <VisitCard 
+                visits={visits}
+              />
+            }
+          </div>
         </div>
       }
     </>
--- src/components/cardFilter/cardFilter.jsx
+++ src/components/cardFilter/cardFilter.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import TextField from '@mui/material//TextField';
 
-const cardFilter = () => {
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+import UseStylesCardFilter from './useStylesCardFilter';
+
+const CardFilter = () => {
+  UseStylesCardFilter();
+
   return (
-    <div className='filter-wrapper'>
-      cardFilter
-    </div>
+    // <div className='filter-wrapper'>
+    //   <TextField 
+    //     sx={{width: '80vw', margin: '30px auto', background: 'white', borderRadius: '5px'}}
+    //     label='Search...'
+    //     fullWidth
+    //     type='search'
+    //   />
+    // </div>
+    
+    <Paper
+      component="form"
+      sx={{ marginTop: '20px', p: '2px 4px', display: 'flex', alignItems: 'center', width: '50vw' }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search..."
+        inputProps={{ 'aria-label': 'search' }}
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   )
 }
 
-export default cardFilter
+export default CardFilter
