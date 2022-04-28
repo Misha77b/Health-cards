a8e8e2e19a5c845e0d597a63f46fa41bc04c69c2
Миша Бабенко <misha77b@gmail.com>
2022-04-28 23:55:09 +0300

    checking possible problems of incorrect work of deploy

--- src/components/VisitCard/VisitCard.jsx
+++ src/components/VisitCard/VisitCard.jsx
@@ -7,23 +7,23 @@ const VisitCard = ({ visits, searchFilter, searchByDoctorValue,  searchByUrgency
   return (
     <>
       {visits.map((visit) => {
-        const searchByFullName = visit.fullName.toLowerCase().includes(searchFilter.toLowerCase());
-        const searchByDoctor = visit.doctor.includes(searchByDoctorValue);
-        const searchByUrgency = visit.urgency.includes(searchByUrgencyValue);
+        const searchByFullName = visit?.fullName.toLowerCase().includes(searchFilter.toLowerCase());
+        const searchByDoctor = visit?.doctor.includes(searchByDoctorValue);
+        const searchByUrgency = visit?.urgency.includes(searchByUrgencyValue);
         if(searchByFullName && searchByDoctor && searchByUrgency) {
           return <div key={visit.id}> 
             <Accordion 
-              fullName={visit.fullName}
-              visitPurpose={visit.visitPurpose}
-              visitShortDescription={visit.visitShortDescription}
-              urgency={visit.urgency}
-              doctor={visit.doctor}
-              bloodPressure={visit.bloodPressure}
-              massBodyIndex={visit.massBodyIndex}
-              prevIllnesses={visit.prevIllnesses}
-              age={visit.age}
-              lastVisitDate={visit.lastVisitDate}
-              id={visit.id}
+              fullName={visit?.fullName}
+              visitPurpose={visit?.visitPurpose}
+              visitShortDescription={visit?.visitShortDescription}
+              urgency={visit?.urgency}
+              doctor={visit?.doctor}
+              bloodPressure={visit?.bloodPressure}
+              massBodyIndex={visit?.massBodyIndex}
+              prevIllnesses={visit?.prevIllnesses}
+              age={visit?.age}
+              lastVisitDate={visit?.lastVisitDate}
+              id={visit?.id}
             />
           </div>
         }
